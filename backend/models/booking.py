c6837f92c4ecbd39f4a885d2bc3c2044db945d46
kate828 <katepenziy10@gmail.com>
2026-05-21 22:37:27 +0300

    Update booking.py

--- backend/models/booking.py
+++ backend/models/booking.py
@@ -4,7 +4,6 @@ import datetime
 
 class Booking(Base):
     __tablename__ = "bookings" 
-
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, ForeignKey('users.id')) 
     session_id = Column(Integer, ForeignKey('sessions.id')) 
